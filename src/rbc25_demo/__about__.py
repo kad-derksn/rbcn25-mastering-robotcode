d96085a3b535e1f0365ad1e8fda71cb350a2e107
klaasderks <61226892+kad-derksn@users.noreply.github.com>
2025-02-12 19:36:09 +0000

    "eerste commit"

---
+++ src/rbc25_demo/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present klaasderks <61226892+kad-derksn@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025-present klaasderks <61226892+kad-derksn@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
